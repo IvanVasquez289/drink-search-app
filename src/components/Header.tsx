a2739fb11dff7861c6d4cf42db22f2012404af4d
ivan <ivanjv1234@gmail.com>
2024-08-07 22:36:29 -0600

    feat: layout with react router

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+
+const Header = () => {
+  return (
+    <header className="bg-slate-800">
+        <div className="mx-auto container px-5 py-16">
+            <div className="flex justify-between">
+                <div>
+                    <img src="/logo.svg" alt="imagen logo" className="w-32" />
+                </div>
+                <nav>
+                    
+                </nav>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header

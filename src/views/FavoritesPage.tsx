c8176a8d54bfe347b4844b8ddcd2dbcf2e5c5709
ivan <ivanjv1234@gmail.com>
2024-08-13 23:42:14 -0600

    feat: favorites page

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,8 +1,29 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 const FavoritesPage = () => {
+  const favorites = useAppStore(state => state.favorites)
+  const hasFavorites = useMemo(() => favorites.length , [favorites])
   return (
-    <div>
-        <h1>Favorites</h1>
-    </div>
+    <>
+        <h1 className="text-6xl font-extrabold">Favoritos</h1>
+        {hasFavorites ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10">
+            {favorites.map(drink => (
+              <DrinkCard
+                key={drink.idDrink}
+                drink={drink}
+              />
+            ))}
+          </div>
+
+        ) : (
+          <p className="text-center text-2xl py-10">
+            Tus bebidas favoritas apareceran aqui
+          </p>
+        )}
+    </>
   )
 }
 
